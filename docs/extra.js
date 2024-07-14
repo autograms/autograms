50e7fe081e62581bbb81386ff6e36f9e02e775a8
ben <Ben Krause>
2024-07-14 15:05:15 -0400

    preparing for initial release

--- docs/extra.js
+++ docs/extra.js
@@ -1,12 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-
+function checkHomePage() {
   // Check if the current page is the home page
   const pathname = window.location.pathname;
 
-
   // Specific path for the home page
-  if (pathname === '/autograms' || pathname === '/autograms/') {
+  if (pathname === '/autograms' || pathname === '/autograms/' || pathname === '/autograms/index.html') {
 
     document.body.classList.add('home-page');
   } 
+  
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  checkHomePage();
+});
+
+window.addEventListener("pageshow", function(event) {
+
+  checkHomePage();
 });
